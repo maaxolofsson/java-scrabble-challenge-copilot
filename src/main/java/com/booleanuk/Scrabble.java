700b167c914733c871665963964810b7adda170e
Max Olofsson <max.olofsson@se.experis.com>
2024-10-25 11:13:49 +0200

    Init commit

---
+++ src/main/java/com/booleanuk/Scrabble.java
@@ -0,0 +1,12 @@
+package com.booleanuk;
+
+public class Scrabble {
+    public Scrabble(String word) {
+
+    }
+
+    public int score() {
+        return -1;
+    }
+
+}
---
+++ src/test/java/com/booleanuk/core/ScrabbleCoreTest.java
@@ -0,0 +1,49 @@
+package com.booleanuk.core;
+
+import com.booleanuk.Scrabble;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ScrabbleCoreTest {
+    @Test
+    public void shouldGive0ForEmptyWords() {
+        Scrabble scrabble = new Scrabble("");
+        Assertions.assertEquals(0, scrabble.score());
+    }
+
+    @Test
+    public void shouldGive0ForWhiteSpace() {
+        Scrabble scrabble = new Scrabble("\n\r\t\b\f");
+        Assertions.assertEquals(0, scrabble.score());
+    }
+
+    @Test
+    public void shouldScore1ForA() {
+        Scrabble scrabble = new Scrabble("a");
+        Assertions.assertEquals(1, scrabble.score());
+    }
+
+    @Test
+    public void shouldScore4ForF() {
+        Scrabble scrabble = new Scrabble("f");
+        Assertions.assertEquals(4, scrabble.score());
+    }
+
+    @Test
+    public void shouldScore6ForStreet() {
+        Scrabble scrabble = new Scrabble("street");
+        Assertions.assertEquals(6, scrabble.score());
+    }
+
+    @Test
+    public void shouldScore22ForQuirky() {
+        Scrabble scrabble = new Scrabble("quirky");
+        Assertions.assertEquals(22, scrabble.score());
+    }
+
+    @Test
+    public void shouldScore41ForCaseInsensitiveWord() {
+        Scrabble scrabble = new Scrabble("OXyPHEnBUTaZoNE");
+        Assertions.assertEquals(41, scrabble.score());
+    }
+}
